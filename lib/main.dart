8ecd95bfa88c473bb8e04badee921b5b348a1018
EDGAR_EMILIO_MERAZ_ACOSTA <a.21308051280388@cbtis128.edu.mx>
2024-02-29 18:56:06 +0000

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,88 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar Edgar Meraz",
       theme: ThemeData(
-        // useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
-}
+} //fin de AppMiTapBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//statefull
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("TabBar Edgar Emilio"),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "Juegos",
+              icon: Icon(Icons.games),
+            ),
+            Tab(
+              text: "Amigos",
+              icon: Icon(Icons.account_box),
+            ),
+            Tab(
+              text: "Tienda",
+              icon: Icon(Icons.shop),
+            ),
+            Tab(
+              text: "Envios",
+              icon: Icon(Icons.send),
+            ),
+          ] //fin de tab
+              ), //Fin Bottom TabBar
         ),
-      ),
+        body: TabBarView(children: const <Widget>[
+          Center(
+            child: Text(
+              "Bloodborne",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Pepito68",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Local 04 en Calle Uva",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Ciudad de mexico, Av. Justicia 3433",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+        ] //Jardin de niños
+            ), //fin de TabBarView
+      ), //fin de scafold
     );
-  }
-}
+  } //fin widget
+} //MiPaginaInicialState
